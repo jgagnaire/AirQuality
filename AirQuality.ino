bc4cc470c4bf4b1e72b1f47d2306339312ab9505
jg <jean.gagnaire@gmail.com>
2017-08-22 15:48:42 +0200

    used Serial1 18 & 19 pins instead of SoftwareSerial with 2 & 3

--- AirQuality.ino
+++ AirQuality.ino
@@ -3,18 +3,17 @@
 #include <Ethernet.h>
 #include <TinyGPS++.h>
 #include <EthernetClient.h>
-#include <SoftwareSerial.h>
 #include <MutichannelGasSensor.h>
 
-static SoftwareSerial serial_connection(2, 3);
 static TinyGPSPlus gps;
 static char mac_address[] = {0xCA, 0xFE, 0xBA, 0xBE, 0xBE, 0xEF};
 static IPAddress client_ip[] = {192, 168, 1, 210}; // if DHCP fails to assign us an IP
-static IPAddress server_ip[] = {192, 168, 1, 69}; // no DNS
+static IPAddress server_ip[] = {192, 168, 1, 64}; // no DNS
 //static char server_name[] = "localhost"; // using DNS
 static EthernetClient client;
 static bool is_online = false;
 static bool data_to_send = false;
+static bool sd_init = false;
 static File datafile;
 static unsigned long previous_time = 0;
 static unsigned long time_interval = 300000; // <=> 5 minutes
@@ -22,7 +21,7 @@ static unsigned long time_interval = 300000; // <=> 5 minutes
 #ifdef SERVER_PORT
 # undef SERVER_PORT
 #endif // !SERVER_PORT
-#define SERVER_PORT 8000
+#define SERVER_PORT 80
 
 #ifdef DATAFILE_NAME
 # undef DATAFILE_NAME
@@ -39,7 +38,7 @@ static void setup_no2_sensor()
 
 static void setup_gps_module()
 {
-  serial_connection.begin(9600);
+  Serial1.begin(9600);
   Serial.println("GPS init OK!");
 }
 
@@ -48,18 +47,29 @@ static void setup_ethernet_module()
   if (!Ethernet.begin(mac_address))
   {
     Serial.println("Ethernet configuration with DCHP failed, trying without...");
-    Ethernet.begin(mac_address, client_ip);
+    if (!Ethernet.begin(mac_address, client_ip))
+      {
+        Serial.println("Ethernet offline");
+        return ;
+      }
   }
   delay(1000); // leave 1 second for the Ethernet controller to be online
   Serial.println("Ethernet online OK");
   if (client.connect(server_ip, SERVER_PORT) == 1)
-    Serial.println("Connected to the server!");
+    Serial.println("Connected to server!");
+  else
+    Serial.println("Could not connect to server");
 }
 
 static void setup_sd_module()
 {
   if (SD.begin(4))
+  {
+    sd_init = true;
     Serial.println("SD init OK!");
+  }
+  else
+    Serial.println("Failed to init SD");
 }
 
 void setup()
@@ -86,30 +96,17 @@ static String create_query_string()
   return query;
 }
 
-static String create_host_string()
-{
-  String host;
-
-  host.concat("Host: ");
-  host.concat(server_ip[0]);
-  host.concat(server_ip[1]);
-  host.concat(server_ip[2]);
-  host.concat(server_ip[3]);
-  host.concat(":");
-  host.concat(SERVER_PORT);
-  return host;
-}
-
 static void send_data()
 {
   client.println(create_query_string());
-  client.println(create_host_string());
-  client.println("Connection: close");
   client.println();
 }
 
 static bool open_datafile()
 {
+  if (!sd_init)
+    setup_sd_module();
+
   if (!datafile)
   {
      datafile = SD.open(DATAFILE_NAME, FILE_WRITE);
@@ -145,8 +142,6 @@ static bool send_sd_data()
     if (ret == '\n' && buf.length() > 0)
     {
       client.println(buf);
-      client.println(create_host_string());
-      client.println("Connection: close");
       client.println();
       buf.remove(0);
     }
@@ -174,8 +169,8 @@ void loop()
       && client.connect(server_ip, SERVER_PORT) && send_sd_data())
         data_to_send = false;
 
-  while (serial_connection.available())
-    gps.encode(serial_connection.read());
+  while (Serial1.available())
+    gps.encode(Serial1.read());
   
   if (gps.location.isValid())
   {
